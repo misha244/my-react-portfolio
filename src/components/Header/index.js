31dda7692e97b2e073bc2c902bc714329b44e59c
Misha <73886737+misha244@users.noreply.github.com>
2021-08-01 20:32:24 +0100

    add img swap

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,26 @@
 import Image from "react-bootstrap/Image";
 import mishabnw from "./mishabnw.png";
+import extrasbnw from "./extrasbnw.png";
 
 import "./Header.css";
+
+function changeImage(e) {
+  e.target.src = extrasbnw;
+}
+function changeBack(e) {
+  e.target.src = mishabnw;
+}
 
 const Header = () => {
   return (
     <header className="header-container">
       <div className="image-container">
-        <Image src={mishabnw} fluid></Image>
+        <Image
+          src={mishabnw}
+          fluid
+          onMouseOver={changeImage}
+          onMouseLeave={changeBack}
+        ></Image>
       </div>
     </header>
   );
